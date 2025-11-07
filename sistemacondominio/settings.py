a1ae09d3f9f9252d7b4e84f92cee71b56fb176bc
root <root@Dell.localdomain>
2025-11-07 20:41:54 -0300

    Configuração de banco de dados Postgresql

--- sistemacondominio/settings.py
+++ sistemacondominio/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from dotenv import load_dotenv
 from pathlib import Path
-import os
-
+import os, dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,10 +85,7 @@ WSGI_APPLICATION = 'sistemacondominio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
 
 
