b8edc1ac476515c29b085958285071781db57d33
root <root@Dell.localdomain>
2025-11-07 21:54:05 -0300

    Ajuste no uso do banco de dados

--- sistemacondominio/settings.py
+++ sistemacondominio/settings.py
@@ -85,7 +85,11 @@ WSGI_APPLICATION = 'sistemacondominio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default='DATABASE_URL',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
