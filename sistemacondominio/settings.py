57640abce53e6060ff9f607c7d25ee678b0edec2
Diego_Linux <dision_1@hotmail.com>
2025-11-12 09:12:37 -0300

    Ajuste nos endereços de APIs

--- sistemacondominio/settings.py
+++ sistemacondominio/settings.py
@@ -36,16 +36,23 @@ ALLOWED_HOSTS = [
     'localhost'
     ]
 
+CORS_ALLOWED_ORIGINS = [
+    'https://condominio.website',
+    'https://sistema-f3fa.onrender.com',
+    'http://localhost:8000'
+    ]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'logon',
 ]
 
@@ -58,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'sistemacondominio.urls'
